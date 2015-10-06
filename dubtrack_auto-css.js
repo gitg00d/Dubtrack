74397f9a370a577102140ab1604ccaf4665b7961
Netox005 <Netox005@users.noreply.github.com>
2015-10-06 18:58:12 -0300

    Update dubtrack_auto-css.js

--- dubtrack_auto-css.js
+++ dubtrack_auto-css.js
@@ -1,2 +1,2 @@
-        $("head").append('<style src="https://rawgit.com/Netox005/Test/master/dubtrack.css"/>');
-        $(".user-header-menu").append('<li><button style="font-size: 1em;height: 32px;margin-right: 16px;border-radius: 0.1875em;">Test</button></li>');
+$("head").append('<style src="https://rawgit.com/Netox005/Test/master/dubtrack.css"/>');
+$(".user-header-menu").append('<li><button style="font-size: 1em;height: 32px;margin-right: 16px;border-radius: 0.1875em;">Test</button></li>');
