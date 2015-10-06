889f3f1183b849c56792c49f2019b5d6517f37a7
Netox005 <Netox005@users.noreply.github.com>
2015-10-06 19:05:02 -0300

    Update dubtrack_auto-css.js

--- dubtrack_auto-css.js
+++ dubtrack_auto-css.js
@@ -1,10 +1,10 @@
 $("head").append('<style src="https://rawgit.com/Netox005/Test/master/dubtrack.css"/>');
 
 var mousePress = false;
-var dragVideoEvent = function() {
+var dragVideoEvent = function(e) {
     if(!mousePress) return;
     console.log("test");
 }
 $(".left_section").onmousemove(dragVideoEvent);
-$(".left_section").onmousedown(function() { mousePress = true; });
-$(".left_section").onmouseup(function() { mousePress = false; });
+$(".left_section").onmousedown(function(e) { mousePress = true; });
+$(".left_section").onmouseup(function(e) { mousePress = false; });
