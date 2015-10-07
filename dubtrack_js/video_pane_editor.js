9eb2826fe1208a29cdd08395349aac2bc2065ece
Netox005 <Netox005@users.noreply.github.com>
2015-10-06 23:50:10 -0300

    Update video_pane_editor.js

--- dubtrack_js/video_pane_editor.js
+++ dubtrack_js/video_pane_editor.js
@@ -1,7 +1,7 @@
 $("head").append('<style src="https://rawgit.com/Netox005/Test/master/dubtrack.css"/>');
 
 $("head").append('<style>.no-selection,#vidEditDiv{webkit-user-select:none;user-select:none;-moz-user-select:none;-ms-user-select:none;}.left_section:not(fullscreenLeft),#vidEditDiv{position:fixed!important;}#vidEditDiv{cursor:pointer;}</style>');
-$(".user-header-menu").prepend('<li><button id="vidEditBtn" style="font-size:.75em;height:32px;margin-right:16px;border-radius:0.1875em;z-index:999998;" onclick="videoEdit()">Video Pane Editor</button></li>');
+$(".user-header-menu").prepend('<li><button id="vidEditBtn" style="font-size:.75em;height:32px;margin-right:16px;border-radius:0.1875em;z-index:999999;" onclick="videoEdit()">Video Pane Editor</button></li>');
 $("#main-room").append('<div id="vidEditDiv" style="background:rgba(255, 190, 0, 0.7);top:0;left:0;display:none;"></div>')
 
 $("#main-menu-left").css("z-index", 999998); // hü3
@@ -17,7 +17,7 @@ function videoEdit(e) {
 
     if(editMode) $(document).click();
 
-    $("#vidEditBtn").css("box-shadow", editMode ? "inherit" : "inset 0 -2px lightgreen !important");
+    $("#vidEditBtn").css("box-shadow", editMode ? "inherit" : "inset 0 -2px lightgreen");
     vidEditDiv.css("display", editMode ? "none" : "block");
 
     editMode = !editMode;
@@ -33,6 +33,11 @@ $(document).mousemove(function(e) {
 
     var vidPos = video.position();
     var x = vidPos.left + (e.pageX - lastMousePos.x), y = vidPos.top + (e.pageY - lastMousePos.y);
+
+    if(x < 0) x = 0;
+    if(y < 0) y = 0;
+    if(x > $(document).width() - video.outerWidth()) x = $(document).width() - video.outerWidth();
+    if(y > $(document).height() - video.outerHeight()) x = $(document).height() - video.outerHeight();
 
     video.css("left", x + "px");
     video.css("top", y + "px");
