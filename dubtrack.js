373e3bdd0d25c21ad04d6399ca6ad332a597b126
Netox005 <Netox005@users.noreply.github.com>
2015-10-06 22:30:46 -0300

    Update dubtrack.js

--- dubtrack.js
+++ dubtrack.js
@@ -1,8 +1,10 @@
 $("head").append('<style src="https://rawgit.com/Netox005/Test/master/dubtrack.css"/>');
 
 $("head").append('<style>.no-selection,#vidEditDiv{webkit-user-select:none;user-select:none;-moz-user-select:none;-ms-user-select:none;}.left_section:not(fullscreenLeft),#vidEditDiv{position:fixed!important;}#vidEditDiv{cursor:pointer;}</style>');
-$(".user-header-menu").prepend('<li><button id="vidEditBtn" style="font-size: 1em;height: 32px;margin-right: 16px;border-radius: 0.1875em;" onclick="videoEdit()">Video Edit</button></li>');
+$(".user-header-menu").prepend('<li><button id="vidEditBtn" style="font-size:1em;height:32px;margin-right:16px;border-radius:0.1875em;z-index:999998;" onclick="videoEdit()">Video Edit</button></li>');
 $("#main-room").append('<div id="vidEditDiv" style="background:rgba(255, 190, 0, 0.7);top:0;left:0;display:none;"></div>')
+
+$("#main-menu-left").css("z-index", 999998);
 
 var video = $(".left_section"), vidEditBtn = $("#vidEditBtn"), vidEditDiv = $("#vidEditDiv");
 var editMode = false;
@@ -15,8 +17,8 @@ vidEditDiv.height(video.outerHeight());
 function videoEdit(e) {
     if(editMode) $(document).click();
 
-    $("#vidEditBtn").css("background", !editMode ? "white" : "inherit");
-    $("#vidEditBtn").css("color", !editMode ? "black" : "inherit");
+    $("#vidEditBtn").css("background", editMode ? "inherit" : "white");
+    $("#vidEditBtn").css("color", editMode ? "inherit" : "black");
     vidEditDiv.css("display", editMode ? "none" : "block");
 
     editMode = !editMode;
