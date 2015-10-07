d8f34c3bab6206472a19e2d3716bb6f2ce5e6090
Netox005 <Netox005@users.noreply.github.com>
2015-10-06 21:29:17 -0300

    Update dubtrack.js

--- dubtrack.js
+++ dubtrack.js
@@ -1,8 +1,8 @@
 $("head").append('<style src="https://rawgit.com/Netox005/Test/master/dubtrack.css"/>');
+$("head").append('<style>.left_section:not(fullscreenLeft){position:fixed;cursor:pointer;webkit-user-select:none;user-select:none;-moz-user-select:none;-ms-user-select:none;}</style>');
+$(".user-header-menu").prepend('<li><button style="font-size: 1em;height: 32px;margin-right: 16px;border-radius: 0.1875em;">Video Drag Mode</button></li>');
 
 var video = $(".left_section");
-video.css("position", "fixed");
-video.css("cursor", "pointer");
 var mousePress = false, wasMoved = false;
 var lastMousePos = { x: -1, y: -1 };
 $(document).on("mousemove", function(e) {
