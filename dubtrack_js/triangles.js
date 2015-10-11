d25264bac9afbbc883f790a31eadddfe69ec9285
Netox005 <Netox005@users.noreply.github.com>
2015-10-11 19:27:55 -0300

    Update triangles.js

--- dubtrack_js/triangles.js
+++ dubtrack_js/triangles.js
@@ -4,12 +4,13 @@ function initTriangle() {
     if(triangles_to !== null) clearTimeout(to);
     if(triangles_init && triangles_target !== null) {
         var width = rnd(25, 300), tr = $('<div class="triangle"></div>').appendTo(triangles_target);
+        tr.css("position", 'fixed');
         tr.css("border-bottom-width", width + 'px');
         tr.css("border-left-width", (width / 2) + 'px');
         tr.css("border-right-width", (width / 2) + 'px');
         tr.css("border-bottom-color", 'rgba(' + rnd(0, 255) + ',' + rnd(0, 255) + ',' + rnd(0, 255) + ',.6)');
         tr.css("top", $(window).height() + 'px');
-        tr.css("left", rnd(0, $(window).width() - width) + 'px')
+        tr.css("left", rnd(0, $(window).width() - width) + 'px');
 
         tr.animate({ top: -width + 'px' }, rnd(250, 2000), "linear", function() { tr.remove(); });
         console.log(tr);
